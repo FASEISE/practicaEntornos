6847909c23e4c23e2aa2bfce5824eb99bdac5d61
FASEISE <128413054+FASEISE@users.noreply.github.com>
2023-03-20 19:28:38 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int variable=10;
+		
+  String nombre= "Rafael";
+		 
+		
+			if (variable<0) {
+				System.out.println("Su numero es negativo");
+			}
+				
+	}	
+
+
+}
